a9dd58d8fc5b5687d536d7e34dac9b14abf585a6
Alexander Sjösten <alexander.sjosten@lexicon.se>
2024-06-20 11:15:32 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function createStudent() {
+    const student = await prisma.student.create({
+        data: {
+            name: "Maria",
+            email: "maria@lexicon.se",
+        },
+    });
+    console.log(student);
+}
+
+async function addCourse() {
+    const course = await prisma.course.create({
+        data: {
+            name: "Programming",
+            teacherId: "clxmxzarc0001wiwypqpqbtyh",
+        },
+    });
+    console.log(course);
+}
+
+async function addStudentToCourse() {
+    const course = await prisma.course.findUnique({
+        where: {
+            id: "clxmyi1u3000131w1b8auief2",
+        },
+    });
+    const student = await prisma.student.findUnique({
+        where: {
+            id: "clxmye3z30000yw610yd8ojrg",
+        },
+    });
+    await prisma.course.update({
+        where: {
+            id: course?.id,
+        },
+        data: {
+            students: {
+                connect: {
+                    id: student?.id,
+                },
+            },
+        },
+    });
+}
+
+addStudentToCourse();
+
 
